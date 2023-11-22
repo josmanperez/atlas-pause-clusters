15d6423ea719d1d3d905649bced65331d541c95f
Josman Pérez <josman.perez@mongodb.com>
2023-11-22 11:22:07 +0000

    * Added support for keep_until

--- functions/getClusters.js
+++ functions/getClusters.js
@@ -21,14 +21,45 @@ exports = function() {
     const ejson_body = EJSON.parse(response.body.text());
     if (ejson_body.results != null) {
       ejson_body.results.forEach(cluster => {
-        if ( (cluster.name != null) && (!cluster.paused) ) {
-          // Pause cluster
-          context.functions.execute('modifyCluster', cluster.name);
+        if ((cluster.name != null) && (!cluster.paused) ) {
+          console.log(`For ${cluster.name}`);
+          var date;
+          if (cluster.tags != null) {
+            let hasKeepUntil = cluster.tags.some(tag => {
+              if (tag.key === 'keep_until' || tag.key === 'Keep_until') {
+                date = new Date(tag.value);
+              return true;
+              }
+            return false;
+            });
+            if (hasKeepUntil) {
+              console.log('> has keep_until tag');
+              if (date instanceof Date && !isNaN(date)) {
+                if (new Date() > date) {
+                  console.log(`> the tag date is before today, cluster ${cluster.name} will be paused`);
+                  pauseCluster(cluster.name);
+                } else {
+                  console.log(`> the tag date is still valid, cluster ${cluster.name} won't be paused`);
+                }
+              }
+            } else {
+              console.log(`> cluster ${cluster.name} will be paused`);
+              pauseCluster(cluster.name);
+            }
+          } else {
+            console.log(`> cluster ${cluster.name} will be paused`);
+            pauseCluster(cluster.name);
+          }
         } 
       });
     }
   }).catch(err => {
     console.error(err);
   });
+  
+  
+  function pauseCluster(name) {
+    context.functions.execute('modifyCluster', name);
+  }
   
 };
